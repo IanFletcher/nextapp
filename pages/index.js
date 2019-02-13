6008785759687442437aa393faf9e472c8c8918a
Ian Fletcher <ianfletcher9@gmail.com>
2019-02-13 16:06:33 +1100

    start of grid

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/navbar'
 import PostLink from '../components/postLink'
+import Link from 'next/link'
 
 const Index = () => (
   <div>
@@ -7,6 +8,15 @@ const Index = () => (
     <PostLink id='fletcher-new-url' title='Fletcher URL'>
       <p> hello world! </p>
     </PostLink>
+    <Link href="/newgrid"> 
+      <a>grid time!</a>
+    </Link>
+    <Link href="/newgrid"> 
+      <a>grid time!</a>
+    </Link>
+    <Link href="/playingfield"> 
+      <a>playing field!</a>
+    </Link>
   </div>
 )
 
---
+++ pages/newgrid.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${props => props.numColumns}, 1fr);
+  background-color: #2196F3;
+`
+
+const newgrid = (props) => {
+  let numColumns = 5
+  let ind = 0
+  let playerColumn = [...Array(numColumns).keys()].map(item => {
+    return (
+      <div key={item + 1}>{item + 1}</div>
+    )
+  })
+  return (
+    <div>
+      <Title>
+        Here It is
+      </Title>
+      <Content numColumns={numColumns}>
+        {playerColumn}
+      </Content>
+    </div>
+  )
+}
+
+export default newgrid
